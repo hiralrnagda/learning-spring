7c8b0563320bc430ad2ba6302650680c6c4a54d1
Hiral <hiralrnagda@gmail.com>
2023-03-15 20:59:45 -0400

    first commit

---
+++ src/main/java/com/hiral/learningspring/data/GuestRepository.java
@@ -0,0 +1,8 @@
+package com.hiral.learningspring.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+}
---
+++ src/main/java/com/hiral/learningspring/data/ReservationRepository.java
@@ -0,0 +1,11 @@
+package com.hiral.learningspring.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    Iterable<Reservation> findReservationByReservationDate(Date date);
+}
